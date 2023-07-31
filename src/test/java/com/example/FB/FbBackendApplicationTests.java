338e8d59d7857bf1c3b92c60a93c3b8f34810413
Finnb <finnb@freenet.de>
2023-07-31 20:11:36 +0200

    setup environment

---
+++ src/test/java/com/example/FB/FbBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FbBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
