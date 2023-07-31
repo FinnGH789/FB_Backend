ff60fc0c2c740a4836c9576bc6a36101f42e79b2
Finnb <finnb@freenet.de>
2023-07-31 20:42:10 +0200

    setup env

---
+++ src/main/java/com/example/FB/Person.java
@@ -0,0 +1,47 @@
+package com.example.FB;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "table_Person")
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "person_name")
+    private String name;
+    @Column
+    private int age;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Person(String name, int age) {
+        super();
+        this.name = name;
+        this.age = age;
+
+
+    }
+}
