e01e3a378df4ae67786f32252a6f7423950f3556
Finnb <finnb@freenet.de>
2023-08-17 21:38:52 +0200

    crossorigin annotation addet, sehr wichtig also merkts euch, jz können wir mitm frontend reden

--- src/main/java/com/example/FB/Controller/PersonController.java
+++ src/main/java/com/example/FB/Controller/PersonController.java
@@ -1,16 +1,13 @@
 package com.example.FB.Controller;
 
 import com.example.FB.Entity.Person;
-import com.example.FB.Repository.PersonRepo;
 import com.example.FB.Service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:4200") // --> Wichtig
 @RestController
 public class PersonController {
     @Autowired
